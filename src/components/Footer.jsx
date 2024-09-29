8d133ff95b91f26f3e74c97832fc36505cef589f
reham-elsayed <rehamshipl445@gmail.com>
2024-09-29 03:51:14 +0200

    f

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import { assets } from '../assets/assets/assets_frontend/assets'
 import useScrollToTop from '../components/ScrollTopHook';
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
@@ -9,7 +9,7 @@ import DoctorDatabaseContextProvider from '../context/DoctorDatabaseContext.jsx'
 import AppointmentContextProvider from '../context/AppointmentContext.jsx'
 import AvailableSlotsContextProvider from '../context/AvailableSlotsContext.jsx'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
 
     <AppContextProvider>
       <AuthContextProvider>
